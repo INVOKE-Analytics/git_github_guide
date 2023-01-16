d09f1a9ade0a78728886addbcd60c9832f334153
Ammar Azman <88831990+Ammar-Azman@users.noreply.github.com>
2023-01-16 08:35:38 +0800

    Add github_project.py

---
+++ github_project.py
@@ -0,0 +1,14 @@
+def full_information(name, age):
+  full_info = f"My name is {name} and I {age} years old"
+  if age < 12:
+    school_info = "I am primary school student"
+  elif 12 < age < 17:
+    school_info = "I am secondary school student"
+  else:
+    school_info = "I am a college student"
+    
+   return full_info + " " + school_info
+    
+name = input("What is your name?") 
+age = input("How old are you?") 
+full_information(name, age) 
