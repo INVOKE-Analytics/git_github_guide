258e5cc5da77dab3764a335c4df24e8ca11b7b1d
alyaaminuddin <alya@invokeisdata.com>
2023-04-06 11:13:10 +0800

    gender changed

--- github_project.py
+++ github_project.py
@@ -1,11 +1,11 @@
 def full_information(name, age):
-  full_info = f"My name is {name} and I {age} years old"
+  full_info = f"My name is <name> and I am <age> years old"
   if age < 12:
     school_info = "I am primary school student"
   elif 12 < age < 17:
     school_info = "I am secondary school student"
   else:
-    school_info = "I am boy"
+    school_info = "I am woman"
     
   return full_info + " " + school_info
     
