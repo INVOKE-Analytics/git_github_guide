ee150aaef37bd12034f65a14d0ddad6e65627bf9
arinanordin <arinamastura@gmail.com>
2023-04-06 11:13:37 +0800

    test

--- github_project.py
+++ github_project.py
@@ -12,6 +12,6 @@ def full_information(name, age):
 name = input("What is your name?") 
 age = input("How old are you?") 
 full_information(name, age) 
-print('hello')
-print ('123')
+print('test')
+
 
