880a66e5d2b4713cc5efdbfb652389a9ead55a81
safwanshamsir99 <safwanshamsir7177@gmail.com>
2023-05-11 16:13:19 +0800

    Adding file and edit something

--- github_project.py
+++ github_project.py
@@ -2,7 +2,7 @@ def full_information(name, age):
   full_info = f"My name is {name} and I {age} years old"
   if age < 12:
     school_info = "I am primary school student"
-  elif 12 < age < 17:
+  elif 12 < age < 18:
     school_info = "I am secondary school student"
   else:
     school_info = "I am boy"
@@ -13,3 +13,4 @@ name = input("What is your name?")
 age = input("How old are you?") 
 full_information(name, age) 
 print('hello')
+print("hehe")
