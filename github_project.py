aed492c12390f64d580a7c76b3c1ed99f6049cbe
Ammar Azman <muhammarsafuan@gmail.com>
2023-05-11 16:13:13 +0800

    Adding file and edit something

--- github_project.py
+++ github_project.py
@@ -1,15 +1,17 @@
 def full_information(name, age):
-  full_info = f"My name is {name} and I {age} years old"
-  if age < 12:
-    school_info = "I am primary school student"
-  elif 12 < age < 17:
-    school_info = "I am secondary school student"
-  else:
-    school_info = "I am boy"
-    
-  return full_info + " " + school_info
-    
-name = input("What is your name?") 
-age = input("How old are you?") 
-full_information(name, age) 
-print('hello')
+    full_info = f"My name is {name} and I {age} years old"
+    if age < 12:
+        school_info = "I am primary school student"
+    elif 12 < age < 17:
+        school_info = "I am secondary school student"
+    else:
+        school_info = "I am boy"
+
+    return full_info + " " + school_info
+
+
+name = input("What is your name?")
+age = input("How old are you?")
+full_information(name, age)
+print("Ammar")
+print("testing")
