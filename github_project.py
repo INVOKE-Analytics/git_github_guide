362a59f89effee24725d01f5b15d99d9b1ab3d0f
Ammar-Azman <muhammarsafuan@gmail.com>
2023-11-21 12:04:44 +0800

    feat: add ammar

--- github_project.py
+++ github_project.py
@@ -1,3 +1,10 @@
 name = input("What is your name? -> ")
 age = input("How old are you? -> ")
 print(f"My name is {name}, I am {age} years old.")
+
+
+print("Ammar")
+print("Ammar")
+print("Ammar")
+print("Ammar")
+print("Ammar")
