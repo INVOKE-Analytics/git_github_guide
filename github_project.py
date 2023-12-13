c3e711b3747c6a6b53fd1912a85b9515cc50bdd8
0732sta <atslhnr99@gmail.com>
2023-12-13 11:31:47 +0800

    add: my name

--- github_project.py
+++ github_project.py
@@ -3,5 +3,5 @@ age = input("How old are you? -> ")
 print(f"My name is {name}, I am {age} years old.")
 
 print("Whats up!")
-print("my name is Putri")
+print("my name is Atsilah")
 print("Hi, this is Ammar")
