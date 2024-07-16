13d9ba107887914731592f191ef14c1ad355d20f
Safwan Shamsir <103097574+safwanshamsir99@users.noreply.github.com>
2024-07-16 12:40:05 +0800

    Update github_project.py

--- github_project.py
+++ github_project.py
@@ -3,7 +3,7 @@ age = input("How old are you? -> ")
 print(f"My name is {name}, I am {age} years old.")
 
 print("Whats up!")
-print("my name is Safwan")
+print("my name is Hamli")
 
 
 def this_is_important_function():
